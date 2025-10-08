8609fe2cc873ce31f58b7c20c8ebfe0b08525925
christiannataren <cnatarnlaguna@byupathway.edu>
2025-10-08 14:03:13 -0400

    discovery

---
+++ chamber/data/places.mjs
@@ -0,0 +1,62 @@
+export const places = [
+    {
+        "title": "Statue of Liberty & Ellis Island",
+        "address": "Liberty Island, New York, NY 10004",
+        "description": "Visit one of the most iconic landmarks in the world. Take a ferry to Liberty Island and Ellis Island to learn about America’s immigration history and enjoy breathtaking views of Manhattan.",
+        "photo_url": "liberty.jpg"
+    },
+    {
+        "title": "Central Park",
+        "address": "New York, NY 10024",
+        "description": "An urban oasis in the heart of Manhattan. Enjoy walking trails, rowing on the lake, picnicking on the Great Lawn, or visiting the Central Park Zoo.",
+        "photo_url": "central-park.jpg"
+    },
+    {
+        "title": "Empire State Building",
+        "address": "20 W 34th St, New York, NY 10001",
+        "description": "Take in panoramic views of the city from the Empire State Building’s 86th-floor observatory. A must-visit attraction offering unforgettable skyline views day and night.",
+        "photo_url": "empire-bulding.jpg"
+    },
+    {
+        "title": "Brooklyn Bridge",
+        "address": "Brooklyn Bridge, New York, NY 10038",
+        "description": "Walk or bike across the Brooklyn Bridge for stunning views of Manhattan and Brooklyn. The bridge is one of NYC’s most photographed landmarks.",
+        "photo_url": "brooklyn.jpg"
+    },
+    {
+        "title": "Times Square",
+        "address": "Manhattan, NY 10036",
+        "description": "Experience the bright lights and energy of Times Square — the city’s entertainment hub, packed with theaters, restaurants, and street performers.",
+        "photo_url": "times-square.jpg"
+    },
+    {
+        "title": "The Metropolitan Museum of Art (The Met)",
+        "address": "1000 5th Ave, New York, NY 10028",
+        "description": "Explore one of the largest and most prestigious art museums in the world, featuring over 5,000 years of art from across the globe.",
+        "photo_url": "the-met.jpg"
+    },
+    {
+        "title": "Broadway Shows",
+        "address": "Theater District, Manhattan, NY 10036",
+        "description": "Enjoy a world-class performance in the heart of the Theater District. From timeless classics to modern hits, Broadway has something for everyone.",
+        "photo_url": "broadway.jpg"
+    },
+    {
+        "title": "High Line Park",
+        "address": "New York, NY 10011",
+        "description": "A unique elevated park built on a historic freight rail line, featuring gardens, art installations, and great city views.",
+        "photo_url": "high-line.jpg"
+    },
+    {
+        "title": "Fifth Avenue Shopping",
+        "address": "Fifth Avenue, Manhattan, NY",
+        "description": "Shop luxury brands or simply enjoy window shopping along the world-famous Fifth Avenue, home to flagship stores and iconic landmarks like St. Patrick’s Cathedral.",
+        "photo_url": "fifth.jpg"
+    },
+    {
+        "title": "Yankee Stadium",
+        "address": "1 E 161 St, Bronx, NY 10451",
+        "description": "Catch a baseball game at Yankee Stadium, home to the legendary New York Yankees. The stadium also offers guided tours and events year-round.",
+        "photo_url": "yankeemain.jpg"
+    }
+]
---
+++ chamber/scripts/discovery.js
@@ -0,0 +1,57 @@
+import { places } from "../data/places.mjs"
+// console.log(places);
+
+
+function createElement(element, textContent) {
+    let el = document.createElement(element);
+
+    el.textContent = textContent;
+
+    return el;
+}
+
+
+function lastVisit() {
+    let difference = Date.now() - new Date(parseInt(window.localStorage.getItem("last-visit")));
+    console.log(difference);
+    let day = Math.round(difference / 1000 / 60 / 60 / 24);
+    let par = document.createElement("span");
+    par.innerHTML = `You last visited <span class="bold red">${day}</span> days ago.`;
+    console.log(par.outerHTML);
+    document.querySelector("#last-visit").appendChild(par);
+    let button = document.createElement("button");
+    button.textContent = "❌";
+    document.querySelector("#last-visit").appendChild(button);
+    window.localStorage.setItem("last-visit", new Date("2025-10-05T10:30:00Z").getTime());
+    button.addEventListener("click", function () {
+        document.querySelector("#last-visit").classList.add("ocultar");
+    })
+}
+
+function buildingSection(place) {
+    let section = document.createElement("section");
+    let img = document.createElement("img");
+    img.setAttribute("alt", `photo of ${place.title}`);
+    img.setAttribute("src", "images/" + place.photo_url);
+    img.setAttribute("loading", "lazy");
+    section.appendChild(img);
+    section.appendChild(createElement("h2", place.title))
+    section.appendChild(createElement("address", place.address));
+    section.appendChild(createElement("p", place.description));
+    return section;
+
+}
+
+
+
+function fillSections() {
+    let container = document.querySelector("#discovery-container");
+    // console.log(container.childNodes);
+    places.map(place => container.appendChild(buildingSection(place)));
+
+}
+
+
+
+fillSections();
+lastVisit();
